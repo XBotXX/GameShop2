f85825a9bb4bd6446cfce5be78b7438e1e58fc5f
XBotXX <1029XBotXX@users.noreply.github.com>
2020-06-22 21:15:03 +0300

    GameShop

---
+++ Registration_Tren/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Registration_Tren
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Entities context = new Entities();
+        public MainWindow()
+        {
+            InitializeComponent();
+            Login.Focus();
+        }
+
+        private void SignIn_Click(object sender, RoutedEventArgs e)
+        {
+            if(context.Users.Where(i => i.Login.Equals(Login.Text) && i.Password.Equals(Pass.Password)).ToList().Any())
+            {
+                FormVideo formVideo = new FormVideo();
+                formVideo.Show();
+                this.Close();
+            }
+            else
+            {
+                Login.BorderBrush = Brushes.Red;
+                Pass.BorderBrush = Brushes.Red;
+            }
+        }
+
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
---
+++ Registration_Tren/UserControlCreate.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Registration_Tren
+{
+    /// <summary>
+    /// Логика взаимодействия для UserControlCreate.xaml
+    /// </summary>
+    public partial class UserControlCreate : UserControl
+    {
+        Entities context = new Entities();
+        ObservableCollection<string> games = new ObservableCollection<string>();
+        public UserControlCreate()
+        {
+            InitializeComponent();
+            Games.ItemsSource = context.Games.ToList();
+            Podborka.ItemsSource = context.Games.ToList();
+        }
+
+        private void GotFocusSearch(object sender, RoutedEventArgs e) => SeachGames.Clear();
+
+        private void LostFocusSearch(object sender, RoutedEventArgs e) => SeachGames.Text = "Поиск";
+
+        private void BigIcon_MouseEnter(object sender, MouseEventArgs e) => ((Label)e.OriginalSource).Foreground = Brushes.White;
+
+        private void BigIcon_MouseLeave(object sender, MouseEventArgs e) => ((Label)e.OriginalSource).Foreground = new SolidColorBrush(Color.FromRgb(84, 172, 255));
+
+        private void BigIcon_PreviewMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            string label = ((Label)sender).Name.ToString();
+            MessageBox.Show(label);
+            if (label.Equals("Big"))
+            {
+
+            }
+            else if (label.Equals("Sred"))
+            {
+                MessageBox.Show("OK1");
+            }
+            else if (label.Equals("Smoll"))
+            {
+                MessageBox.Show("OK2");
+            }
+        }
+    }
+}
